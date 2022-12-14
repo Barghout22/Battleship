bcdf374013e3629e84888fd9c689ca4372c7a982
Mahmoud <barghoutmahmoud2@gmail.com>
2022-12-14 02:15:37 +0200

    adjusted some mistake in gameboard factory. the ship creator wasn't actually concating the new coordinates into itself which was causing issues in all the following functions and with the search for hit coordinates search.

--- src/srcCode/GameboardFactory.js
+++ src/srcCode/GameboardFactory.js
@@ -1,22 +1,32 @@
 import { shipFactory } from "./shipFactory";
 export const Gameboard = () => {
-  let attackedCoordinates = {};
+  let hits = [];
+  let misses = [];
   let groupOfShips = [];
   const createShip = ([x, y], length) => {
     let dummyShip = [[x, y]];
-    for (let i = 0; i < length; i++) {
-      dummyShip.concat([x + i, y]);
+    for (let i = 1; i < length; i++) {
+      dummyShip = dummyShip.concat([[x + i, y]]);
     }
     groupOfShips.push([shipFactory(length), dummyShip]);
   };
-  const receiveAttack = (coor) => {
-    attackedCoordinates.append(coor);
-    for (i = 0; i < groupOfShips.length; i++) {}
+  const receiveAttack = (x, y) => {
+    for (let i = 0; i < groupOfShips.length; i++) {
+      groupOfShips[i][1].forEach((coor) => {
+        if (x == coor[0] && y == coor[1]) {
+          groupOfShips[i][0].isHit();
+          hits.concat([[x, y]]);
+          return;
+        }
+      });
+      misses.concat([[x, y]]);
+      return;
+    }
   };
   const allShipsSunk = () => {
     let returnValue = true;
     groupOfShips.forEach((ship) => {
-      returnValue = returnValue && ship.isSunk();
+      returnValue = returnValue && ship[0].isSunk();
     });
     return returnValue;
   };
--- src/srcCode/GameboardFactory.test.js
+++ src/srcCode/GameboardFactory.test.js
@@ -1,7 +1,24 @@
 import { Gameboard } from "./GameboardFactory";
 
 const testGameboard = Gameboard();
-testGameboard.createShip([1,2],4);
+testGameboard.createShip([1, 2], 4);
+
+
 test("are all ships sunk?", () => {
-  expect(testGameboard.allShipsSunk()).toBe(true);
+  expect(testGameboard.allShipsSunk()).toBe(false);
+});
+
+
+
+describe("testing if all ships are sunk after hits", () => {
+  beforeEach(() => {
+    testGameboard.receiveAttack(1, 2);
+    testGameboard.receiveAttack(2, 2);
+    testGameboard.receiveAttack(3, 2);
+    testGameboard.receiveAttack(4, 2);
+    return;
+  });
+  test("are all ships sunk now ?", () => {
+    expect(testGameboard.allShipsSunk()).toBe(true);
+  });
 });
