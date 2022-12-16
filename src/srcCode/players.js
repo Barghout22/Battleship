1039bea7c272b501a9afced5bc8e9de20006e6bc
Mahmoud <barghoutmahmoud2@gmail.com>
2022-12-16 21:05:36 +0200

    adjusted the player move function. adjusted to receive and check the validity of regular player moves as well as ai. added tests for this accordingly to make sure the function doesn't accept any repititive values

--- src/srcCode/players.js
+++ src/srcCode/players.js
@@ -1,14 +1,12 @@
 import { Gameboard } from "./GameboardFactory";
 export const player = (playerName, shipsInput) => {
-  const playerName = playerName;
+  const thisPlayerName = playerName;
   const thisGameboard = Gameboard();
   let moves = [];
-  if (playerName !== "AI") {
-    for (let i = 0; i < shipsInput.length; i++) {
-      thisGameboard.createShip(shipsInput[i][0], shipsInput[i][1]);
-    }
-  } else {
-  }
+
+  const checkShipPlacementValidity = ([x, y], length) => {
+    return "valid";
+  };
 
   const checkMoveValidity = ([x, y]) => {
     for (let i = 0; i < moves.length; i++) {
@@ -17,7 +15,27 @@ export const player = (playerName, shipsInput) => {
     return "valid";
   };
 
-  const playerAttack = () => {
+  if (playerName !== "AI") {
+    for (let i = 0; i < shipsInput.length; i++) {
+      thisGameboard.createShip(shipsInput[i][0], shipsInput[i][1]);
+    }
+  } else {
+    for (let i = 0; i < shipsInput.length; i++) {
+      let randomShipPlacement;
+      do {
+        randomShipPlacement = [
+          Math.floor(Math.random() * 10 + 1),
+          Math.floor(Math.random() * 10 + 1),
+        ];
+      } while (
+        checkShipPlacementValidity(randomShipPlacement, shipsInput[i]) ===
+        "invalid"
+      );
+      thisGameboard.createShip(randomShipPlacement, shipsInput[i]);
+    }
+  }
+
+  const playerAttack = ([x, y]) => {
     let playerMove;
     if (playerName === "AI") {
       do {
@@ -26,12 +44,19 @@ export const player = (playerName, shipsInput) => {
           Math.floor(Math.random() * 10 + 1),
         ];
       } while (checkMoveValidity(playerMove) === "invalid");
+      moves = moves.concat([playerMove]);
+      return playerMove;
     } else {
-      //playerMove = requestMove();
+      if (checkMoveValidity([x, y]) === "valid") {
+        moves = moves.concat([[x, y]]);
+        return true;
+      } else return false; //playerMove = requestMove();
     }
-    moves = moves.concat([playerMove]);
-    return playerMove;
   };
 
-  return { playerName, playerAttack };
+  const checkMoves = () => {
+    return moves;
+  };
+
+  return { thisPlayerName, playerAttack, checkMoves };
 };
--- src/srcCode/players.test.js
+++ src/srcCode/players.test.js
@@ -1,3 +1,25 @@
 import { player } from "./players";
 
-const testPlayer=player("AI",)
+const playerTestMoves = [
+  [[1, 1], 2],
+  [[5, 2], 4],
+  [[4, 3], 3],
+];
+const testPlayer1 = player("Mahmoud", playerTestMoves);
+const testPlayer2 = player("AI", [5, 4, 3, 2, 1]);
+
+testPlayer1.playerAttack([1, 2]);
+testPlayer1.playerAttack([3, 4]);
+
+console.log(testPlayer1.checkMoves());
+
+// test("ai attacking:", () => {
+//   expect(testPlayer2.playerAttack()).toBe([5, 1]);
+// });
+
+test("player attacking:", () => {
+  expect(testPlayer1.playerAttack([1, 3])).toBe(true);
+});
+test("player attacking:", () => {
+  expect(testPlayer1.playerAttack([1, 2])).toBe(false);
+});
