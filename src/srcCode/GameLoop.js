763aa118e93b40813a98ff475ead344e98df1ff5
Mahmoud <barghoutmahmoud2@gmail.com>
2022-12-17 14:00:12 +0200

    started creating the gameloop and function dom modules

--- src/srcCode/GameLoop.js
+++ src/srcCode/GameLoop.js
@@ -1,1 +1,8 @@
 import { player } from "./players";
+import { intiateGameboards } from "./domInteraction";
+const GameLoop = (playerName, ShipArrangement) => {
+  const player1 = player(playerName, ShipArrangement);
+  const AI = player("AI", ShipArrangement);
+  let playerTurn = player1.playerName;
+  intiateGameboards(player1);
+};
